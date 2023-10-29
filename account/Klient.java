7a30e878cd82829c80984fc4933d01f0355eeb87
maliaaska <asmilewski@gmail.com>
2023-10-29 17:47:21 +0100

    the account set up

---
+++ account/Klient.java
@@ -0,0 +1,10 @@
+package com.milewskiarkadiuszmodul8.account;
+
+public class Klient {
+    private String imie;
+    private String nazwisko;
+    private int rokurodzienie;
+    private String email;
+    private String telefon;
+
+}
