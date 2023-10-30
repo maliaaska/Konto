ed6b71bd7e16f9f73ca90d65f98ffab5898b3c18
maliaaska <asmilewski@gmail.com>
2023-10-30 16:51:58 +0100

    Dodanie klasy konto do projektu. All works fine as of now

---
+++ account/Konto.java
@@ -0,0 +1,38 @@
+package com.milewskiarkadiuszmodul8.account;
+
+public class Konto {
+    private double stanKonta = 1223.86;
+    private final boolean debet = false;
+
+
+
+    public double getAccountBalance() {
+          return stanKonta;
+    }
+    public boolean isDebet() {
+        return false;
+    }
+
+
+    public String setNumerRachunku(String numer) {
+        return numer;
+    }
+
+    public void setWplacSrodki(double kwota) {
+        this.stanKonta = stanKonta + kwota;
+///// dlaczego ta metoda nie dziala przy przypisuwaniu jej do nowego double (patrz klasa Konto)
+    }
+
+    public void setWyplacSrodki(double kwota) {
+        if (debet) {
+            this.stanKonta = stanKonta + kwota;
+            System.out.println(" Twój obecny stan konta po wyplacie to " + kwota);
+        } else if ( kwota > this.stanKonta){
+            System.out.println("Nie masz wystarczających środków na koncie. Prosimy spróbować póżniej");
+        } else {
+            this.stanKonta = stanKonta - kwota;
+            System.out.println(" Twój obecny stan konta po wyplacie to " + kwota);
+        }
+
+    }
+}
--- account/Main.java
+++ account/Main.java
@@ -1,7 +1,32 @@
 package com.milewskiarkadiuszmodul8.account;
 
+import com.milewskiarkadiuszmodul6.banking.Konto;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        com.milewskiarkadiuszmodul6.banking.Konto konto = new Konto();
+
+        System.out.println("Wprowadź numer swojego konta skladajacego sie z 16-stu cyfr");
+        String numerKonta = scanner.nextLine();
+        konto.setNumerRachunku(numerKonta);
+        double stanKonta = konto.getAccountBalance();
+        boolean debet = konto.isDebet();
+
+        System.out.println("Twoj numer rachunku to " + numerKonta + " i obecny stan konta to " + stanKonta);
+        System.out.println("Konto posiada debet: " + debet);
+
+        System.out.println("Podaj ile chcesz wpłacić środków?");
+        double kwota = scanner.nextDouble();
+        konto.setWplacSrodki(kwota);
+//        11111 // Dlaczego w tym przypadku nie pobiera zauktualizowanego stanu konta?
+        System.out.println(konto.getAccountBalance());
+
+        System.out.println("Napisz ile chciałbyś wypłacić pieniędzy.");
+        kwota = scanner.nextDouble();
+        konto.setWyplacSrodki(kwota);
         // Konstruktor
         Adres adres = new Adres("Malczewskiego", "00-113", "Warszawa", 14);
 
